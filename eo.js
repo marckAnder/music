c8289c2a7f0d93adaf98450557a3971f82a29419
marckAnder <138138163+marckAnder@users.noreply.github.com>
2023-06-29 10:43:22 -0500

    Add files via upload

---
+++ eo.js
@@ -0,0 +1,11 @@
+const audioPlayer = document.getElementById('audio-player');
+const playBtn = document.getElementById('play-btn');
+const pauseBtn = document.getElementById('pause-btn');
+
+playBtn.addEventListener('click', () => {
+  audioPlayer.play();
+});
+
+pauseBtn.addEventListener('click', () => {
+  audioPlayer.pause();
+});
